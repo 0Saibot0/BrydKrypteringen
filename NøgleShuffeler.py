eb22c5ac5a42a7ff3a6f06c159d28545c5678bbe
0Saibot0 <99321452+0Saibot0@users.noreply.github.com>
2022-09-06 11:19:08 +0200

    kypt

--- NøgleShuffeler.py
+++ NøgleShuffeler.py
@@ -1,4 +1,5 @@
 import random
 alfabetet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z','Æ','Ø','Å']
+
 random.shuffle(alfabetet)
 print(alfabetet)
